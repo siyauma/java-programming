4deea937568429e744ebc41675ec32f3e07d13f3
siyauma <43057623+siyauma@users.noreply.github.com>
2018-09-07 12:29:19 +0530

    Add files via upload

---
+++ mathcalculation.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    double b,c,d,e,f;
+    a=8;
+    b=5;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("The sum of two numbers is:"+c);
+    System.out.println("The difference of two no is:"+d);
+    System.out.println("The product of two no is :"+e);
+    System.out.println("The division of two no is:"+f);
+  }
+}
