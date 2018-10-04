595c8452b4349df533d9b716d7e6ee3266b4cd71
siyauma <43057623+siyauma@users.noreply.github.com>
2018-10-04 11:55:23 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int number=3;
+        if(number > 0)
+        {
+            System.out.println(number+" is a positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println(number+" is a negative number");
+        }
+        else
+        {
+            System.out.println(number+" is neither positive nor negative");
+        }
+    }
+}
