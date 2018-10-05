0689279188562583f123a841a122635fbd9802ff
siyauma <43057623+siyauma@users.noreply.github.com>
2018-10-05 12:02:35 +0530

    Add files via upload

---
+++ largest3numbers.java
@@ -0,0 +1,16 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        double n1 = 8, n2 = 16, n3 = 20;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
